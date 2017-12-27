cbbd296221ad6b09ac1914125ff60b2277e3bf58
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-27 10:48:32 +0100

    2- graphic of the first viewController with segues to register and login

---
+++ Pods/Target Support Files/Pods-Rate|Saga/Pods-Rate|Saga-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rate_Saga : NSObject
+@end
+@implementation PodsDummy_Pods_Rate_Saga
+@end
--- Rate|Saga/Controller/ViewController.swift
+++ Rate|Saga/Controller/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
     }
 
     override func didReceiveMemoryWarning() {
