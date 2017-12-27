76c5f437abaec39a081e8e8b7736f0524cbba617
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-27 12:44:41 +0100

    3 - created three controller for the login and register functions

--- Rate|Saga/Controller/RegisterViewController.swift
+++ Rate|Saga/Controller/RegisterViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
+    
+    //IBOutlet
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +27,20 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+        SVProgressHUD.show()
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    SVProgressHUD.dismiss()
+                    
+                }
+            }
+        }
     }
-    */
-
-}
--- Rate|Saga/Controller/ViewController.swift
+++ Rate|Saga/Controller/ViewController.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Rende trasparente il navigationController
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
         self.navigationController?.view.backgroundColor = UIColor.clear
+        self.navigationController?.navigationBar.tintColor = UIColor.flatYellow()
     }
 
     override func didReceiveMemoryWarning() {
