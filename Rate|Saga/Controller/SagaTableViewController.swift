069b9a3dec52cf9461547f78939de556aeb8b889
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-31 18:38:49 +0100

    8 - added background to SagaTableviewController

--- Rate|Saga/Controller/SagaTableViewController.swift
+++ Rate|Saga/Controller/SagaTableViewController.swift
@@ -14,6 +14,8 @@ class SagaTableViewController: UITableViewController {
     
     @IBOutlet weak var navBarTitle: UINavigationItem!
     
+    let backgroundImage = UIImage(named: "starwars-mask")
+
     let sagaListRef:DatabaseReference = Database.database().reference().child("sagasList")
     
     var sagaListItems:[SagaList] = []
@@ -21,6 +23,11 @@ class SagaTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navBarTitle.title = "Saga|List"
+        tableView.separatorStyle = .none
+        let imageView = UIImageView(image: backgroundImage)
+        imageView.contentMode = .scaleAspectFill
+        self.tableView.backgroundView = imageView
+
 
 //        print("inizio caricamento saghe")
         //codice tratto da testFirebase di HidranArias
@@ -54,6 +61,9 @@ class SagaTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SagaCell", for: indexPath)
         let saga = sagaListItems[indexPath.row]
         cell.textLabel?.text = saga.saga
+        
+        //colore del testo delle celle
+        cell.textLabel?.textColor = UIColor.white
 
         return cell
     }
@@ -71,55 +81,12 @@ class SagaTableViewController: UITableViewController {
         }
     }
     
-    //MARK: - Tabbar Methods
+    // MARK: - Tableview UI Methods
     
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = .clear
+    }
     
-    
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
